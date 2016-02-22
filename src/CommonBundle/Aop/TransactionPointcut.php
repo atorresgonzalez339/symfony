571f18c23ad786d7987067ad1c815ddde65b8a05
Angel Torres <atorres@reloquest.com>
2016-02-21 23:04:12 -0500

    Add AOP to common bundle

---
+++ src/CommonBundle/Aop/TransactionPointcut.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @copyright (c) 2013, RALF
+ * @author  Livan L. Frometa Osorio <llfrometa@gmail.com>
+ * @version 1.0.0
+ */
+
+namespace CommonBudnle\Aop;
+
+use Doctrine\Common\Annotations\Reader;
+use JMS\AopBundle\Aop\PointcutInterface;
+
+
+class TransactionPointcut implements PointcutInterface {
+
+    private $reader;
+
+    function __construct(Reader $reader) {
+        $this->reader = $reader;
+    }
+
+    public function matchesClass(\ReflectionClass $class) {
+        return false !== strpos($class->name, 'Business');
+    }
+
+    public function matchesMethod(\ReflectionMethod $method) {
+        return null !== $this->reader->getMethodAnnotation($method, 'CommonBundle\Annotations\Transactional');
+    }
+
+}
+
