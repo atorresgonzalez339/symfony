98f5f5fa0de3bdb530d87367778f72bd5609eaaf
Liván Frómeta <llfrometa@gmail.com>
2016-02-27 22:48:00 -0500

    Date piker

--- src/UserBundle/Form/ProfileType.php
+++ src/UserBundle/Form/ProfileType.php
@@ -8,6 +8,8 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use UserBundle\DBAL\Types\GenderType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class ProfileType extends AbstractType
 {
@@ -16,8 +18,20 @@ class ProfileType extends AbstractType
         $builder
             ->add('first_name')
             ->add('last_name')
-            ->add('gender')
-            ->add('birth_date')
+            ->add('gender', ChoiceType::class, array(
+                'choices' => GenderType::getChoices(),
+                'attr' => array('class' => 'select2 validate'),
+                'empty_data' => null,
+                'required'    => false,
+                'placeholder' => 'Select Gender',
+            ))
+            ->add('birth_date',null,array(
+                'widget' => 'single_text',
+                'attr' => array(
+                    'type'=>'text',
+                ),
+                'required' => false,
+            ))
             ->add('cell_phone')
             ->add('office_phone')
             ->add('email')
