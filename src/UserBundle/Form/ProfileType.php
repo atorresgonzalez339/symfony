24d156fcef5135c575cb392eba5e9462a9069900
Angel Torres <atorres@reloquest.com>
2016-02-28 22:56:03 -0500

    Last changes

--- src/UserBundle/Form/ProfileType.php
+++ src/UserBundle/Form/ProfileType.php
@@ -5,9 +5,6 @@ namespace UserBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use UserBundle\DBAL\Types\GenderType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
