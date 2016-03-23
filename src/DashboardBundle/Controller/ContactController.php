f434c13d1f73cfd08b21d575c65b6c1e84cef07d
Liván Frómeta <llfrometa@gmail.com>
2016-03-22 22:45:55 -0500

    Create contact functionality

--- src/DashboardBundle/Controller/ContactController.php
+++ src/DashboardBundle/Controller/ContactController.php
@@ -6,7 +6,6 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
-
 use APY\DataGridBundle\Grid\Source\Entity;
 use APY\DataGridBundle\Grid\Action\DeleteMassAction;
 
@@ -16,9 +15,15 @@ class ContactController extends Controller{
  	 * @Route("/contacts", name="contact_index")
  	 */
   public function indexAction(Request $request){
-
-    return $this->render('DashboardBundle:Contact:index.html.twig');
-
+      $source = new Entity('DashboardBundle:Contact');
+      $grid = $this->get('grid');
+      $grid->setSource($source);
+      $grid->hideColumns(array('id'));
+      $grid->addMassAction(new DeleteMassAction());
+      $grid->setLimits($this->container->getParameter('admin.paginator.limits.config'));
+      if ($request->isXmlHttpRequest()) return $grid->getGridResponse('DashboardBundle:Contact:indexAjax.html.twig');
+      if ($grid->isReadyForRedirect())return new RedirectResponse($grid->getRouteUrl());
+      return $grid->getGridResponse('DashboardBundle:Contact:index.html.twig');
   }
 
 }
--- src/DashboardBundle/Entity/Contact.php
+++ src/DashboardBundle/Entity/Contact.php
@@ -3,10 +3,12 @@
 namespace DashboardBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use APY\DataGridBundle\Grid\Mapping as GRID;
 
 /**
  * @ORM\Entity
- * @ORM\Table(name="plans")
+ * @ORM\Table(name="contact")
+ * @GRID\Source(columns="id,first_name,last_name,email")
  */
 class Contact
 {
@@ -18,96 +20,48 @@ class Contact
     protected $id;
 
     /**
-     * @ORM\Column(type="string", nullable=false)
+     * @ORM\Column(name="first_name",type="string", nullable=false)
      */
     private $first_name;
 
     /**
-     * @ORM\Column(type="string", nullable=false)
+     * @ORM\Column(name="last_name",type="string", nullable=false)
      */
     private $last_name;
 
     /**
-     * @ORM\Column(type="integer", nullable=false)
+     * @ORM\Column(name="email",type="integer", nullable=false)
      */
     private $email;
 
-    /**
-     * Get id
-     *
-     * @return integer 
-     */
-    public function getId()
-    {
+    public function getId() {
         return $this->id;
     }
 
-    /**
-     * Set first_name
-     *
-     * @param string $firstName
-     * @return Contact
-     */
-    public function setFirstName($firstName)
-    {
+    public function setFirstName($firstName) {
         $this->first_name = $firstName;
-
         return $this;
     }
 
-    /**
-     * Get first_name
-     *
-     * @return string 
-     */
-    public function getFirstName()
-    {
+    public function getFirstName() {
         return $this->first_name;
     }
 
-    /**
-     * Set last_name
-     *
-     * @param string $lastName
-     * @return Contact
-     */
-    public function setLastName($lastName)
-    {
+    public function setLastName($lastName) {
         $this->last_name = $lastName;
-
         return $this;
     }
 
-    /**
-     * Get last_name
-     *
-     * @return string 
-     */
-    public function getLastName()
-    {
+    public function getLastName() {
         return $this->last_name;
     }
 
-    /**
-     * Set email
-     *
-     * @param integer $email
-     * @return Contact
-     */
-    public function setEmail($email)
-    {
+    public function setEmail($email) {
         $this->email = $email;
-
         return $this;
     }
 
-    /**
-     * Get email
-     *
-     * @return integer 
-     */
-    public function getEmail()
-    {
+    public function getEmail() {
         return $this->email;
     }
 }
